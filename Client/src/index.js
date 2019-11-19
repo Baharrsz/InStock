1cc86daab79973cae3591461ad5796ee6ac96934
Yuchen Lin <kloselyc@gmail.com>
2019-11-18 17:15:04 -0800

    working version with nothin

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
