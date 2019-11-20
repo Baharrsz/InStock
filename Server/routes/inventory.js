37e2e040ae246e29ab84e35a0430e0876680dcbb
Kayle Odegard <mattiasodegard10@gmail.com>
2019-11-20 14:46:16 -0800

    Modified Inventory route

--- Server/routes/inventory.js
+++ Server/routes/inventory.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const uuid = require("uuid/v4");
 const fs = require("fs");
 const inventoryList = require("../models/inventory.json");
+const filePath = __dirname + "/../models/inventory.json";
 
 function writeJSONFile(filename, content) {
   fs.writeFile(filename, JSON.stringify(content), err => {
@@ -13,6 +14,27 @@ function writeJSONFile(filename, content) {
 
 router.get("/", (req, res) => {
   res.json(inventoryList);
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  const newProduct = {
+    name: req.body.name,
+    description: req.body.description,
+    // descriptionplus: req.body.descriptionplus,
+    date: req.body.date,
+    quantity: req.body.quantity,
+    status: req.body.status,
+    customer: req.body.customer,
+    warehouse: req.body.warehouse,
+    city: req.body.city,
+    country: req.body.country,
+    id: uuid()
+  };
+  // console.log(newProduct);
+  inventoryList.push(newProduct);
+  writeJSONFile(filePath, inventoryList);
+  res.send("Success");
 });
 
 module.exports = router;
