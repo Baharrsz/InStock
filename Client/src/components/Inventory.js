abfafd66ddfb6feb6a69f571f708a964ee9403fc
Bahar Raiszadeh <Bahar@android-587678dcf1f685e5.localdomain>
2019-11-21 11:24:52 -0800

    Basic structure of Inventory Route.

--- Client/src/components/Inventory.js
+++ Client/src/components/Inventory.js
@@ -1,62 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
-
-function TableHeader() {
-  return (
-    <div className="inventory__tableHeader">
-      <div className="inventory__tableHeader-item"> ITEM</div>
-      <div className="inventory__tableHeader-item"> LAST ORDERED</div>
-      <div className="inventory__tableHeader-item"> LOCATION</div>
-      <div className="inventory__tableHeader-item"> QUANTITY</div>
-      <div className="inventory__tableHeader-item"> STATUS</div>
-    </div>
-  );
-}
-
-function TableRow(props) {
-  return (
-    <div className="inventory__Row">
-      <div className="inventory__Row-item">
-        <Link className="inventory__Row-item--name" to={props.id}>
-          {props.name}
-        </Link>
-        <div className="inventory__Row-item--description">
-          {props.description}
-        </div>
-      </div>
-      <div className="inventory__Row-item">{props.date}</div>
-      <div className="inventory__Row-item">
-        {props.city},{props.country}
-      </div>
-      <div className="inventory__Row-item"> {props.quantity}</div>
-      <div className="inventory__Row-item"> {props.status}</div>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
+import InventoryMain from "./subcomponents/InventoryMain";
+import InventoryDetails from "./subcomponents/InventoryDetails";
 
 export default class Inventory extends Component {
-  state = {
-    inventoryList: undefined
-  };
-  componentDidMount() {
-    axios
-      .get("http://localhost:8080/inventory")
-      .then(response => this.setState({ inventoryList: response.data }));
-  }
-
   render() {
-    if (!this.state.inventoryList) return <>Loading...</>;
-    else {
-      let tableRows = this.state.inventoryList.map(product => {
-        return TableRow(product);
-      });
-      return (
-        <div className="inventory__table">
-          <TableHeader />
-          <div className="inventory__Rows">{tableRows}</div>
-        </div>
-      );
-    }
+    return (
+      <Switch>
+        <Route path="/" component={InventoryMain} />
+        {/* <Route
+          path="/:id"
+          render={props => {
+            return <InventoryDetails id={props.match.params.id} />;
+          }}
+        /> */}
+        <Route path="/:id" component={InventoryDetails} />
+      </Switch>
+    );
   }
 }
