507f22afa088d4afa1e210b661492c0f3754803a
Helkate326 <mattiasodegard10@gmail.com>
2019-11-22 00:22:39 -0800

    added some things

--- Client/src/components/Create.js
+++ Client/src/components/Create.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 import axios from "axios";
-import Reactswitch from "react-switch";
+import Switch from "react-switch";
 
 export default class Create extends Component {
+  constructor() {
+    super();
+    this.state = { checked: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.setState({ checked });
+    console.log(checked)
+  }
+
+  // Instock(submit) {
+  //   if (submit === { checked: false }) {
+  //     console.log(submit)
+  //     return "Instock";
+  //   } else {
+  //     return "Not in Stock";
+  //   }
+  // }
+
   uploadSubmit = submit => {
     axios.post("http://localhost:8080/inventory", {
       product: submit.target.product.value,
@@ -10,7 +30,7 @@ export default class Create extends Component {
       city: submit.target.city.value,
       country: submit.target.country.value,
       quantity: submit.target.quantity.value,
-      status: submit.target.status.value,
+      status: this.Instock(this.state),
       description: submit.target.description.value
     });
     submit.target.reset();
@@ -88,16 +108,33 @@ export default class Create extends Component {
           <h4 className="create__container-title silver">STATUS</h4>
           <div className="create__container-flex">
             <label id="label-black">In Stock</label>
-            <Reactswitch />
+            <label className="create__container-switch">
+              <Switch
+                name="status"
+                checked={this.state.checked}
+                onChange={this.handleChange}
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={30}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={20}
+                width={48}
+                className="react-switch"
+                id="material-switch"
+              />
+            </label>
           </div>
         </div>
 
         <div className="create__container">
           <h4 className="create__container-title silver">ITEM DESCRIPTION</h4>
           <input
-            required
             name="description"
-            className="create__container-instock optional"
+            className="create__container-input"
+            id="optional"
             placeholder="(Optional)"
           ></input>
         </div>
