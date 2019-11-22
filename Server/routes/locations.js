e68750c75424ca013d1f91f3877784247d9f8b1b
Yuchen Lin <kloselyc@gmail.com>
2019-11-21 17:20:43 -0800

    finish part of styling for add warehouse page

--- Server/routes/locations.js
+++ Server/routes/locations.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const locationContent = require("../models/locations.json");
+const locationFileName = __dirname + "/../models/locations.json";
 const productInfo = require("../models/inventory.json");
+const json_helper = require("../helper/json_helper");
 
 router.get("/content", (request, response) => {
+  response.send(locationContent);
+});
+
+router.post("/content", (request, response) => {
+  if (request.body) {
+    locationContent.push(request.body);
+  }
+  json_helper.writeJson(locationFileName, locationContent);
   response.send(locationContent);
 });
 
