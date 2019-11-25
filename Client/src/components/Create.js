b027b3ab5cba623866379b10153d1fcaf048bf5b
Bahar Raiszadeh <Bahar@android-587678dcf1f685e5.localdomain>
2019-11-25 14:21:49 -0800

    Create component completed.

--- Client/src/components/Create.js
+++ Client/src/components/Create.js
@@ -1,45 +1,26 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Switch from "react-switch";
-
+​
 export default class Create extends Component {
-  constructor() {
-    super();
-    this.state = { checked: false };
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+      warehouses: undefined,
+      warehouseNames: undefined
+    };
     this.handleChange = this.handleChange.bind(this);
+    this.city = React.createRef();
+    this.country = React.createRef();
   }
-
-  handleChange(checked) {
-    this.setState({ checked });
-    console.log(checked);
-  }
-
-  // Instock(submit) {
-  //   if (submit === true) {
-  //     return "In-Stock";
-  //   } else {
-  //     return "Out Of Stock";
-  //   }
-  // }
-
-  uploadSubmit = submit => {
-    axios.post("http://localhost:8080/inventory", {
-      name: submit.target.name.value,
-      description: submit.target.description.value,
-      date: submit.target.date.value,
-      quantity: submit.target.quantity.value,
-      status: this.state.checked === false ? "Out Of Stock" : "In Stock",
-      warehouse: submit.target.warehouse.value,
-      city: submit.target.city.value,
-      country: submit.target.country.value
-    });
-    submit.target.reset();
-  };
+​
   render() {
-    return (
-
+    return !this.state.warehouses ? (
+      <>Loading...</>
+    ) : (
       <div className="create-background">
-        <form onSubmit={submit => this.uploadSubmit(submit)} className="create">
+        <form onSubmit={this.props.addFunction} className="create">
           <h1 className="create-title">Create New</h1>
           <div className="create-flex">
             {/* Last Product name input */}
@@ -52,7 +33,7 @@ export default class Create extends Component {
                 placeholder="Item Name"
               ></input>
             </div>
-
+​
             {/* Last Ordered input */}
             <div className="create__container">
               <h4 className="create__container-title silver">LAST ORDERED</h4>
@@ -64,7 +45,7 @@ export default class Create extends Component {
               ></input>
             </div>
           </div>
-
+​
           <div className="create-flex">
             {/* Warehouse input */}
             <div className="create__container">
@@ -73,9 +54,10 @@ export default class Create extends Component {
                 name="warehouse"
                 required
                 className="create__container-input"
+                onChange={this.populateWarehouse}
               >
-                <option value="0"></option>
-                <option value="0">Warehouse 1</option>
+                <option value=""></option>
+                {this.state.warehouseNames}
               </select>
             </div>
             {/* City input */}
@@ -84,25 +66,26 @@ export default class Create extends Component {
               <input
                 required
                 name="city"
+                ref={this.city}
                 className="create__container-input"
                 placeholder="City"
               ></input>
             </div>
           </div>
-
+​
           <div className="create-flex">
             {/* Country input */}
             <div className="create__container">
               <h4 className="create__container-title silver">COUNTRY</h4>
               <input
                 required
+                ref={this.country}
                 name="country"
                 className="create__container-input"
-                placeholder="Canada"
+                placeholder="Country"
               ></input>
             </div>
-
-
+​
             {/* Quantity input */}
             <div className="create__container">
               <h4 className="create__container-title silver">QUANTITY</h4>
@@ -114,33 +97,45 @@ export default class Create extends Component {
               ></input>
             </div>
           </div>
-          <div className="create__container__switch">
-            <h4 className="create__container-title silver">STATUS</h4>
-            <div className="create__container-flex" id="instock-flex">
-              <label id="label-black">In Stock</label>
-              <label className="create__container-switch">
-                <Switch
-                  name="status"
-                  checked={this.state.checked}
-                  onChange={this.handleChange}
-                  onColor="#32cd32"
-                  onHandleColor="#ffffff"
-                  handleDiameter={30}
-                  uncheckedIcon={false}
-                  checkedIcon={false}
-                  boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-                  activeBoxShadow="rgba(0, 0, 0, 0.2)"
-                  height={20}
-                  width={48}
-                  className="react-switch"
-                  id="material-switch"
-                value={this.state.checked ? "In Stock" : "Out of Stock"}
-
-                />
-              </label>
+​
+          <div className="create-flex">
+            {/* Customer */}
+            <div className="create__container">
+              <h4 className="create__container-title silver">Ordered By</h4>
+              <input
+                required
+                name="customer"
+                className="create__container-input"
+              ></input>
+            </div>
+​
+            <div className="create__container__switch">
+              <h4 className="create__container-title silver">STATUS</h4>
+              <div className="create__container-flex" id="instock-flex">
+                <label id="label-black">In Stock</label>
+                <label className="create__container-switch">
+                  <Switch
+                    name="status"
+                    checked={this.state.checked}
+                    onChange={this.handleChange}
+                    onColor="#86d3ff"
+                    onHandleColor="#ffffff"
+                    handleDiameter={30}
+                    uncheckedIcon={false}
+                    checkedIcon={false}
+                    boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                    activeBoxShadow="rgba(0, 0, 0, 0.2)"
+                    height={20}
+                    width={48}
+                    className="react-switch"
+                    id="material-switch"
+                    value={this.state.checked ? "In Stock" : "Out of Stock"}
+                  />
+                </label>
+              </div>
             </div>
           </div>
-
+​
           <div className="create__container">
             <h4 className="create__container-title silver">ITEM DESCRIPTION</h4>
             <input
@@ -163,5 +158,45 @@ export default class Create extends Component {
         </form>
       </div>
     );
+  }
+​
+  componentDidMount() {
+    this.getWarehouses();
+  }
+​
+  populateWarehouse = select => {
+    const selectedWarehouse = select.target.value;
+​
+    const warehouseInfo = this.state.warehouses.find(
+      location => location.warehouse === selectedWarehouse
+    );
+    if (warehouseInfo) {
+      this.city.current.value = warehouseInfo.city;
+      this.country.current.value = warehouseInfo.country;
+    } else {
+      this.city.current.value = "";
+      this.country.current.value = "";
+    }
+  };
+​
+  getWarehouses = () => {
+    axios.get("http://localhost:8080/locations/content").then(response => {
+      this.setState({ warehouses: response.data }, () => {
+        const options = this.state.warehouses.map(warehouse => {
+          return (
+            <option value={warehouse.warehouse}>{warehouse.warehouse}</option>
+          );
+        });
+        console.log("options", options);
+​
+        this.setState({ warehouseNames: options });
+      });
+    });
+  };
+​
+  handleChange(checked) {
+    this.setState({ checked });
+    console.log(checked);
+    console.log(this.state.checked);
   }
 }
