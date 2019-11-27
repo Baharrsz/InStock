1fff04d59143ed2ba5abdbd820c9ef5d4ceda748
Helkate326 <mattiasodegard10@gmail.com>
2019-11-26 22:35:33 -0800

    final desktop css changes

--- Client/src/components/subcomponents/InventoryDetails.js
+++ Client/src/components/subcomponents/InventoryDetails.js
@@ -51,150 +51,148 @@ export default class InventoryDetails extends Component {
           </div>
 
           <div className="product__details">
-            <div className="product__container">
-              <label className="product__container__description-label">
-                ITEM DESCRIPTION
-              </label>
-              <textarea
-                className="product__container-input-descriptionplus"
-                name="descriptionPlus"
-                defaultValue={product.descriptionplus}
-                disabled={disabled}
-              />
-            </div>
-
-            <div className="product__contain-flex">
-              <div className="product-flex">
-                <div className="product__container">
-                  <label className="product__container__orderedBy-label">
-                    ORDERED BY
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="customer"
-                    defaultValue={product.customer}
-                    disabled={disabled}
-                  />
-                </div>
-                <div className="product__container">
-                  <label className="product__container__lastOrdered-label">
-                    LAST ORDERED
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="date"
-                    defaultValue={product.date}
-                    disabled={disabled}
-                  />
-                </div>
-
-                <div className="product__container">
-                  <label className="product__container__quantity-label">
-                    QUANTITY
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="quantity"
-                    defaultValue={product.quantity}
-                    disabled={disabled}
-                  />
-                </div>
-                <div className="product__container">
-                  <label className="product__container__categories-label">
-                    CATEGORIES
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="categories"
-                    defaultValue={product.categories}
-                    disabled={disabled}
-                  />
-                </div>
-                <div className="product__container">
-                  <label className="product__container__reference-label">
-                    REFERENCE NUMBER
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="id"
-                    defaultValue={product.id}
-                    disabled={disabled}
-                  />
-                </div>
-
-                <div className="product__container">
-                  <label className="product__container__warehouse-label">
-                    WAREHOUSE
-                  </label>
-                  <select
-                    name="warehouse"
-                    required
-                    className="product__container-input product__container-select"
-                    onChange={this.populateWarehouse}
-                    disabled={disabled}
-                    defaultValue={product.warehouse}
-                  >
-                    {this.state.warehouseNames}
-                  </select>
-                </div>
+            <div className="product__desktop-flex">
+              <div className="product__container">
+                <label className="product__container__description-label product__container-label">
+                  ITEM DESCRIPTION
+                </label>
+                <textarea
+                  className="product__container-input-descriptionplus"
+                  name="descriptionPlus"
+                  defaultValue={product.descriptionplus}
+                  disabled={disabled}
+                />
               </div>
 
-              <div className="product-flex">
-                <div className="product__container">
-                  <label className="product__container__loacation-label">
-                    LOCATION
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="city"
-                    ref={this.city}
-                    defaultValue={`${product.city}`}
-                    disabled
-                  />
-                  <input
-                    className="product__container-input"
-                    name="country"
-                    ref={this.country}
-                    defaultValue={`${product.country}`}
-                    disabled
-                  />
-                </div>
-
-                <div
-                  className="product__container"
-                  style={{
-                    display: this.state.disabled ? "none" : "block"
-                  }}
-                >
-                  <label className="product__container-label">STATUS</label>
-                  <div className="product__container-status-flex">
-                    <label id="isblack">In Stock</label>
-                    <Switch
-                      onChange={this.statusSwitch}
-                      checked={this.state.checked}
-                      onColor="#82B72A"
-                      onHandleColor="#ffffff"
-                      handleDiameter={30}
-                      uncheckedIcon={false}
-                      checkedIcon={false}
-                      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-                      activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-                      height={20}
-                      width={48}
+              <div className="product__contain-flex">
+                <div className="product-flex">
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      ORDERED BY
+                    </label>
+                    <input
+                      className="product__container-input"
+                      name="customer"
+                      defaultValue={product.customer}
+                      disabled={disabled}
                     />
+                  </div>
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      LAST ORDERED
+                    </label>
+                    <input
+                      className="product__container-input"
+                      name="date"
+                      defaultValue={product.date}
+                      disabled={disabled}
+                    />
+                  </div>
+
+                  <div className="product__container">
+                    <label className="product__container-label">QUANTITY</label>
+                    <input
+                      className="product__container-input"
+                      name="quantity"
+                      defaultValue={product.quantity}
+                      disabled={disabled}
+                    />
+                  </div>
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      CATEGORIES
+                    </label>
+                    <input
+                      className="product__container-input"
+                      name="categories"
+                      defaultValue={product.categories}
+                      disabled={disabled}
+                    />
+                  </div>
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      REFERENCE NUMBER
+                    </label>
+                    <input
+                      className="product__container-input"
+                      name="id"
+                      defaultValue={product.id}
+                      disabled={disabled}
+                    />
+                  </div>
+
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      WAREHOUSE
+                    </label>
+                    <select
+                      name="warehouse"
+                      required
+                      className="product__container-input product__container-select"
+                      onChange={this.populateWarehouse}
+                      disabled={disabled}
+                      defaultValue={product.warehouse}
+                    >
+                      {this.state.warehouseNames}
+                    </select>
                   </div>
                 </div>
 
-                <div className="product__container">
-                  <label className="product__container__warehouse-label">
-                    WAREHOUSE NAME
-                  </label>
-                  <input
-                    className="product__container-input"
-                    name="warehouse"
-                    defaultValue={product.warehouse}
-                    disabled={disabled}
-                  />
+                <div className="product-flex">
+                  <div className="product__container">
+                    <label className="product__container-label">LOCATION</label>
+                    <input
+                      className="product__container-input"
+                      name="city"
+                      ref={this.city}
+                      defaultValue={`${product.city}`}
+                      disabled
+                    />
+                    <input
+                      className="product__container-input"
+                      name="country"
+                      ref={this.country}
+                      defaultValue={`${product.country}`}
+                      disabled
+                    />
+                  </div>
+
+                  <div
+                    className="product__container"
+                    style={{
+                      display: this.state.disabled ? "none" : "block"
+                    }}
+                  >
+                    <label className="product__container-label">STATUS</label>
+                    <div className="product__container-status-flex">
+                      <label id="isblack">In Stock</label>
+                      <Switch
+                        onChange={this.statusSwitch}
+                        checked={this.state.checked}
+                        onColor="#82B72A"
+                        onHandleColor="#ffffff"
+                        handleDiameter={30}
+                        uncheckedIcon={false}
+                        checkedIcon={false}
+                        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                        height={20}
+                        width={48}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="product__container">
+                    <label className="product__container-label">
+                      WAREHOUSE NAME
+                    </label>
+                    <input
+                      className="product__container-input"
+                      name="warehouse"
+                      defaultValue={product.warehouse}
+                      disabled={disabled}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
